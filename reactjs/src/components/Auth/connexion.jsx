a38a4f30736b560c3079dfcd5925db18bd2f0610
BouBou <69978359+BouBouw@users.noreply.github.com>
2022-09-13 09:37:06 +0200

    home & connexion

---
+++ reactjs/src/components/Auth/connexion.jsx
@@ -0,0 +1,21 @@
+import './page-style.css';
+
+async function connexionSettings() {
+    let state = null;
+
+    if(state === null) {
+        // register or login
+    } else {
+        // already login
+    }
+}
+
+const Connexion = () => {
+    return(
+        <div className=''>
+            <a onClick={connexionSettings()}></a>
+        </div>
+    )
+}
+
+export default Connexion;
